1ae413c63632b79726a64d3040c8621e43c002ed
Manisha1705 <manishakumariofficial2005@gmail.com>
2024-10-28 23:13:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<!-- Place this before the closing body tag -->
+<script>
+    document.querySelector('.btn-know-more').addEventListener('click', () => {
+        alert("More details about iPhone 16 coming soon!");
+    });
+</script>
